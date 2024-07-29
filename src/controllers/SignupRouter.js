77c4016ec57ec206db861dedd773192541f98ad4
BenDavies1218 <14972@coderacademy.edu.au>
2024-07-29 22:10:52 +1000

    feature/signup express validator

--- src/controllers/SignupRouter.js
+++ src/controllers/SignupRouter.js
@@ -1,28 +1,43 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
 
 // EMAIL FUNCTION
 const { sendEmail } = require("../utils/sendEmail");
 
-// USER MODEL 
+// USER MODEL
 const { UserModel } = require("../models/UserModel");
 
-router.post("/", async (request, response, next) => {
+// VALIDATION RULES
+const signupValidationRules = [
+  check("username")
+    .notEmpty()
+    .withMessage("Username is required")
+    .isLength({ min: 3 })
+    .withMessage("Username must be at least 3 characters long"),
+  check("email").isEmail().withMessage("Invalid email address"),
+  check("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+];
+
+router.post("/", signupValidationRules, async (request, response, next) => {
+  // CHECK THE VALIDATION RULES
+  const errors = validationResult(request);
+  if (!errors.isEmpty()) {
+    return response.status(400).json({ errors: errors.array() });
+  }
+
   try {
     const { username, email, password } = request.body;
-
-    // VERIFY BODY DATA
-    if (!username || !email || !password) {
-      return response
-        .status(400)
-        .json({ error: "Missing details in body request." });
-    }
 
     // CREATE A NEW USER MODEL
     const newUser = await UserModel.create({ username, email, password });
 
     // SEND WELCOME EMAIL
-    sendEmail(email, "welcome", username=);
+    sendEmail(email, "welcome", "no-code", username);
 
     // SEND RESPONSE TO CLIENT
     return response.status(201).json({
